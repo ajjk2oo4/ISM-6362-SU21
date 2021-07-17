d0449b7c1afb529fb0151b72027efa4a3d9c7d3c
KavyaBhat1225 <77723094+KavyaBhat1225@users.noreply.github.com>
2021-07-17 11:24:04 -0700

    Create GCP_CancelJob.py

---
+++ GCP_CancelJob.py
@@ -0,0 +1,7 @@
+#Canceling a job run
+from google.cloud import bigquery
+client = bigquery.Client()
+job_id = 'bq-job-123x456-123y123z123c'  # replace with your job ID
+location = 'us'                         # replace with your location
+
+job = client.cancel_job(job_id, location=location)
